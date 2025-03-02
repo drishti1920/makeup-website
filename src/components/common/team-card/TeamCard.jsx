e962668c1a9c19445c32f4063b34f4ed75165590
Drishti Saraf <drishtisaraf0307@gmail.com>
2025-03-02 16:53:09 +0530

    Enhance TeamCard component with PropTypes and responsive styling

--- src/components/common/team-card/TeamCard.jsx
+++ src/components/common/team-card/TeamCard.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import styles from "./TeamCard.module.css";
 
 const TeamCard = ({ image, name, description, position }) => {
   return (
     <div className={styles.TeamCardContainer}>
       <div className={styles.TeamImage}>
-        <img src={image} alt="image-name" />
+        <img src={image} alt={`${name} - ${position}`} />
       </div>
 
       <div className={styles.TeamDetails}>
@@ -15,6 +16,13 @@ const TeamCard = ({ image, name, description, position }) => {
       </div>
     </div>
   );
+};
+
+TeamCard.propTypes = {
+  image: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  position: PropTypes.string.isRequired
 };
 
 export default TeamCard;
